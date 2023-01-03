d1b1392b3b62a6a3ef8bc57a0a3c35c3713702dd
e2hnoey <e2hnoey@gmail.com>
2023-01-03 16:55:40 +0900

    공지사항 게시판 테이블 및 시퀀스 작성

---
+++ BuyMe/src/main/webapp/sql/cyh.sql
@@ -0,0 +1,16 @@
+-- 공지사항 게시판
+create table notice_board(
+	noti_num number,
+	noti_title varchar2(100) not null,
+	noti_content clob not null,
+	noti_hit number(5) default 0 not null,
+	noti_reg_date date default sysdate not null,
+	noti_modify_date date,
+	noti_link varchar2(90),
+	noti_ip varchar2(40) not null,
+	mem_num number not null,
+	constraint notice_board_pk primary key (noti_num),
+	constraint notice_board_fk foreign key (mem_num) references member (mem_num)
+);
+
+create sequence noticeboard_seq;
