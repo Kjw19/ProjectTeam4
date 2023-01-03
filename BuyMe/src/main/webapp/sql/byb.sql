27c4702f79002db130845266b01559eef32df407
82109 <82109@192.168.0.25>
2023-01-03 17:39:21 +0900

    sql 추가 작업

--- BuyMe/src/main/webapp/sql/byb.sql
+++ BuyMe/src/main/webapp/sql/byb.sql
@@ -1,4 +1,4 @@
---문의게시판 테이블
+--문의게시판 테이블 
 create table REQUEST_BOARD(
  req_num number,
  req_title varchar2(50) not null,
@@ -8,10 +8,11 @@ create table REQUEST_BOARD(
  req_modify_date date not null,
  req_ip varchar2(40) not null,
  req_filename varchar2(150),
- me_num number not null,
+ mem_num number not null,
+ 
  constraint REQUEST_BOARD_pk primary key (req_num),
- constraint REQUEST_BOARD_fk1 foreign key (me_num)
-                            references member (me_num)
+ constraint REQUEST_BOARD_fk1 foreign key (mem_num)
+                            references MEM_NUM (mem_num)
                             
 );
 create sequence requestboard_seq;
