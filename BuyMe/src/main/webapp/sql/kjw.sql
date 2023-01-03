2ca93d0130912fc2d662e9dee2b43cfa2a076974
김재우 <ksg3477@gmail.com>
2023-01-03 16:52:41 +0900

    펀딩게시판 댓글 테이블 SQL및 시퀀스 작성 펀딩게시판 문의 테이블 SQL및 시퀀스 작성

--- BuyMe/src/main/webapp/sql/kjw.sql
+++ BuyMe/src/main/webapp/sql/kjw.sql
@@ -1,3 +1,32 @@
-#Welcome Page
-/main/main.do=kr.main.action.MainAction
+create table fund_comment{
+	comment_num number,
+	comm_content varchar2(300) not null,
+	comm_reg_date date not null,
+	mem_num number not null,
+	fund_num number not null,
+	constraint fund_comment_pk primary key (comment_num),
+	constraint fund_comment_fk foreign key (mem_num)
+                            references member (mem_num),
+    constraint fund_comment_fk foreign key (fund_num)
+                            references fund_board (fund_num)
+};
+create sequence fund_comment_seq;
+
+create table fund_inquiry{
+	inquiry_num number,
+	inqu_title varchar2(150) not null,
+	inqu_content crob not null,
+	inqu_reg_date date not null,
+	re_inqu_is_ok varchar2(500),
+	mem_num number not null,
+	fund_num number not null,
+	constraint fund_inquiry_pk primary key (inquiry_num),
+	constraint fund_inquiry_fk foreign key (mem_num)
+                            references member (mem_num),
+    constraint fund_inquiry_fk2 foreign key (fund_num)
+                            references fund_board (fund_num)
+};
+create sequence fund_inquiry_seq;
+
+
 
