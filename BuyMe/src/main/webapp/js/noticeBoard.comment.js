2b922f89f655fba33f06d6594ff009a6e786d2ec
Secrethan <rladlfgks422@gmail.com>
2023-01-09 15:37:42 +0900

    공지사항게시판 댓글 js 파일수정

--- BuyMe/src/main/webapp/js/noticeBoard.comment.js
+++ BuyMe/src/main/webapp/js/noticeBoard.comment.js
@@ -26,6 +26,34 @@ $(function() {
 			alert('네트워크 오류 발생');
 		}
 	});
+	
+	//댓글 작성 폼 초기화
+	function initForm(){
+		$('textarea').val('');
+		$('#re_first .letter-count').text('300/300');
+	}
+	//textarea에 내용 입력 시 글자수 체크
+	$(document).on('keyup','textarea',function(){
+		//입력한 글자수 구함
+		let inputLength = $(this).val().length;
+		
+		if(inputLength > 300) {
+			$(this).val($(this).val().substring(0,300));
+		}
+		else {
+			let remain = 300 - inputLength;
+			remain += '/300';
+			if($(this).attr('id') == 're_content') {
+				//등록폼 글자 수 
+				$('#re_first .letter-count').text(remain);
+			}
+			else {
+				//수정폼 글자 수
+				$('#mre_first .letter-count').text(remain);
+			}
+		}
+	});
+	
 	//댓글 수정
 	$(document).on('submit', '#mre_form', function(event) {
 		//기본 이벤트 제거
@@ -67,6 +95,59 @@ $(function() {
 			}
 		});
 	});
+	
+	//댓글 수정 버튼 클릭시 수정폼 노출 
+	$(document).on('click', 'modify-btn', function(){
+		//댓글 번호
+		let comment_num = $(this).attr('data-renum');
+		
+		//댓글 내용                                            
+		let comment_content = $(this).parent().find('p').html().replace(/<br>/gi,'\n'); //g:지정문자열 모두 i : 대소문자 무시
+	
+		//댓글 수정폼 UI
+		let modifyUI = '<form id="mre_form">'; 
+		   modifyUI += '<input type="hidden" name="re_num" id="re_num" value="'+comment_num+'">';
+		   modifyUI += '<textarea rows="3" cols="50" name="re_content" id="mre_content" class="rep-content">'+content+'</textarea>';
+		   modifyUI += '<div id="mre_first"><span class="letter-count">300/300</span></div>';
+		   modifyUI += '<div id="mre_second" class="align-right">';
+		   modifyUI += ' <input type="submit" value="수정">';
+		   modifyUI += ' <input type="button" value="취소" class="re-reset">';
+		   modifyUI += '</div>';
+		   modifyUI += '<hr size="1" noshade width="96%">';
+		   modifyUI += '</form>';
+		 
+		   //이전에 이미 수정하는 댓글이 있을 경우 수정버튼을
+		   //클릭하면 숨김 sub-item을 환원시키고 수정폼을 초기화됨
+		   initModifyForm();
+		   
+		   //지금 클릭해서 수정하고자 하는 데이터는 감추기
+		   //수정버튼을 감싸고 있는 div
+		   $(this).parent().hide();
+		   
+		   //수정폼을 수정하고자 하는 데이터가 있는 div에 노출
+		   $(this).parents('.item').append(modifyUI); 
+		   
+		   //입력한 글자수 셋팅
+		   let inputLength = $('#mre_content').val().length;
+		   let remain = 300 - inputLength;
+		   remain += '/300';
+		   
+		   //문서 객체에 반영
+		   $('#mre_first .letter-count').text(remain);
+	});
+	
+	//수정폼에서 취소 버튼 클릭시 수정폼 초기화 
+	$(document).on('click', 're-rest', function(){
+		initModifyForm();
+	});
+	
+	//댓글 수정폼 초기화 
+	function initModifyForm(){
+		$('sub-item').show();
+		$('#mre_form').remove();
+	}
+	
+	
 	//댓글 목록
 	function selectList(pageNum) {
 		currentPage = pageNum;
@@ -127,6 +208,16 @@ $(function() {
 			}
 		});
 	}
+	//페이지 처리 이벤트 연결(다음 댓글 보기 버튼 클릭시 데이터 추가)
+	$('.paging-button input').click(function(){
+		selectList(currentPage + 1);
+	});
+	
+		//페이지 처리 이벤트 연결(다음 댓글 보기 버튼 클릭시 데이터 추가)
+	$('.paging-button input').click(function(){
+		selectList(currentPage + 1);
+	});
+	
 	//댓글 삭제
 	$(document).on('click', 'delete-btn', function() {
 		let notice_num = $(this).attr('data-renum');
